822e277c73733ac9db06dd4b4e26cd9200d30224
Trevor Edris <trevor.edris@gmail.com>
2022-08-12 21:12:02 -0400

    Lots of f strings and things

--- reader.py
+++ reader.py
@@ -1,19 +1,24 @@
 import csv
 
+
 def file_read(file_name):
-    with open(file_name, mode = "r") as csvfile:
+    with open(file_name, "r") as csvfile:
         display_file= csv.reader(csvfile)
-        
         for item in display_file:
             print(item)
-    
-        csvfile.close
+
 
 def header_row(csv_file):
-    csv_list = []
     with open(csv_file, "r") as file:
         read_file = csv.reader(file)
         for row in read_file:
+            return row
+
+
+def get_file_contents(file_name):
+    csv_list = []
+    with open(file_name, "r") as file:
+        read_file = csv.reader(file)
+        for row in read_file:
             csv_list.append(row)
-        file.close
-        return csv_list[0]
+    return csv_list
--- writer.py
+++ writer.py
@@ -1,55 +1,58 @@
 import csv
 import reader
 
-def file_write(new_csv):
 
-    header_row_list = str(input("Enter Header Row using (,) to seperate the columns\n").split(","))
-    with open(input(new_csv), "w") as csvfile:
+def file_write(filename):
+
+    # We are calling split directly on the input because we only care about working with the
+    # input as a list data type.
+    header_row_list = input("Enter Header Row using (,) to seperate the columns\n").split(',')
+    with open(input(filename), "w") as csvfile:
         ex_write = csv.writer(csvfile)
-        ex_write.writerow(header_row_list)
+        ex_write.writerow(",".join(header_row_list))
 
         while True:
+            # We are NOT calling split directly on the input here beacuse we need to check if
+            # the user inputs the exact string 'exit', in which case we need to break out of
+            # the loop.
             user_input = input("Enter row seperated by a comma (,)\nEnter (exit) to quit\n")
             if user_input == "exit":
                 break
+
             row = user_input.split(",")
             if len(header_row_list) == len(row):
                 ex_write.writerow(row)
             else:
-                print("Invalid entry. Try entering " + len(header_row_list) + " columns instead of " + len(row) + " columns.")
+                print(f"Invalid entry. Try entering {len(header_row_list)} columns instead of {len(row)} columns.")
+
 
 def add_person(csv_file):
     reader.file_read(csv_file)
     header_row = reader.header_row(csv_file)
 
     while True:
-        
-        newrow = input("Enter a row to add to the csv file\n").split(",")
+        newrow = input("Enter a row separated by a comma (,) to add to the csv file\n").split(",")
         if len(header_row) == len(newrow):
             with open(csv_file, "a", newline="") as adder:
                 exwrite = csv.writer(adder)
 
                 exwrite.writerow(newrow)
-                adder.close
-                break
+            break
         else:
             print("Invalid format, Try again")
 
+
 def csv_row_editer(csv_file):
-    csv_list = []
-    with open(csv_file, "r") as file:
-        read_file = csv.reader(file)
-        for row in read_file:
-            csv_list.append(row)
-    #print(csv_list)
+    csv_list = reader.get_file_contents(csv_file)
     for i in range(len(csv_list)):
-        print("Row " +  str(i) + ": " + str(csv_list[i]))
-    edit_row = int(input("\nWhich row would you like to change? Enter 1 - " + str(len(csv_list)-1) + ":\n"))
+        print(f'Row {i}: {",".join(csv_list[i])}')
+
+    edit_row = int(input(f"\nWhich row would you like to change? Enter 1 - {len(csv_list)-1}:\n"))
     print( "Please enter the new details for each of the following :")
     for i in range(len(csv_list[0])):
-        newRow = input("Enter new data for " + str(csv_list[0][i]) + " :")
+        newRow = input(f"Enter new data for {csv_list[0][i]}:")
         csv_list[edit_row][i] = newRow
-    print("Row " + str(i) + " :" + str(csv_list[i]))
+    print(f"Row {i}: {csv_list[i]}")
 
     change_csv = input("Do you want to save the changes? Enter (y) or (n)\n").lower()
     print(change_csv)
@@ -59,12 +62,9 @@ def csv_row_editer(csv_file):
             for i in range(len(csv_list)):
                 read_file.writerow(csv_list[i])
 
+
 def csv_column_editer(csv_file):
-    csv_list = []
-    with open(csv_file, "r") as file:
-        read_file = csv.reader(file)
-        for row in read_file:
-            csv_list.append(row)
+    csv_list = reader.get_file_contents(csv_file)
     #print(csv_list)
     for i in range(len(csv_list)):
         print("Row " +  str(i) + ": " + str(csv_list[i]))
